f1c539823b32425687af44028931fadefbecf512
Pierre Colle <piercus@gmail.com>
2016-11-12 18:42:59 +0100

    first working version

--- test/example.js
+++ test/example.js
@@ -5,8 +5,9 @@ var cv = require("opencv");
 salient("assets/mona.png", function(err, saliency){
   // res is an node-opencv floating-point Matrix
   // maximum is 1, minium is 1
-  var imgOutput = new cv.Matrix(saliency.width(), saliency.height);
-  saliency.convertTo(imgOutput, cv.Constants.CV_8U);
+  var width = saliency.width(), height =  saliency.height();
+  var imgOutput = new cv.Matrix(width, height, cv.Constants.CV_8U);
+  saliency.convertTo(imgOutput, cv.Constants.CV_8U, 255);
   imgOutput.save("assets/output.png");
-  console.log('Image saved to ./assets/output.png');
+  //console.log('Image saved to ./assets/output.png');
 });
